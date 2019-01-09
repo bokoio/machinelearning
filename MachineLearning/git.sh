e04f60bec72286cf1a2175431af731bc263af871
Felipe Nedeff <feboscato@gmail.com>
2019-01-09 10:28:08 +0100

    Add Files

---
+++ MachineLearning/git.sh
@@ -0,0 +1,11 @@
+#git config --global user.email "feboscato@gmail.com"
+#git config --global user.name "Felipe Nedeff"
+
+#Initialize the local directory as a Git repository.
+#git init
+git add .
+git commit -m $1
+#git remote add origin https://github.com/bokoio/machinelearning.git
+#git remote -v
+git push origin master
+
